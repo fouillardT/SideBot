3e95b84132527e65b496e894c7b7205007f20d23
Théophane FOUILLARD <theophane.fouillard@etu.univ-nantes.fr>
2020-11-26 21:10:48 +0100

    First Version of the bot

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # SideBot
-This is a discord bot that will basically do coin flips
+
+This is a discord bot that will basically do coin flips.
+The main goal was to help players choose side in games such as LoL, Chess, CSGO etc.
+
+# Discord modules 
+
+Import Discord node modules with 
+
+$ npm install discord.js
+
+then initialize it in the folder that you want with 
+
+$ npm init 
+
+to start the bot, it's as easy as : 
+
+$ node main.js
+
+# Needs
+
+To make it work you'll need a token and some other things. 
+The best I can do is to put the link of the video I began with : https://www.youtube.com/watch?v=j_sD9udZnCk 
+From now it's all about reading discord.js documentation.
+Good luck and have fun tossing coins.
---
+++ main.js
@@ -0,0 +1,122 @@
+const Discord = require('discord.js');
+const Config = require('./config.json');
+const SideBot = new Discord.Client;
+
+const prefix = "!";
+
+const waitingTime = 10000;
+
+
+SideBot.on("message", msg =>{
+    //check if author is a bot
+    if(msg.author.bot) return;
+    //check if message doesn't begin with prefix ! 
+    if(!msg.content.startsWith(prefix));
+
+    //get argument & command used
+    const cmdBody = msg.content.slice(prefix.length);
+    const args = cmdBody.split(' ');
+    const cmd = args.shift().toLowerCase();
+
+    if(cmd == "cf" || cmd == "coinflip"){
+        msg.reply("I'll toss a coin. Wich side do you choose ?\n 💿 = head \n 📀 = tail ").then(sentmsg => {
+            sentmsg.react('💿'); 
+            sentmsg.react('📀');
+
+            const filter = (reaction, user) => {
+                return ['💿','📀'].includes(reaction.emoji.name) && user.id === msg.author.id;
+            };
+
+            sentmsg.awaitReactions(filter,{max:1, time: waitingTime,error: ['time']})
+            .then(collected => {
+                var array = [] ;
+                for(var i = 0; i<1000; i++){
+                    array.push(Math.random());
+                }
+
+                var rdval = array[Math.floor(Math.random()* Math.floor(999))];
+                var tail = rdval < 0.5;
+                var head = rdval > 0.5;
+
+                if(tail){
+                    sentmsg.channel.send("the Result is tail (📀)");
+                } else {
+                    sentmsg.channel.send("the Result is head (💿)");
+                }
+
+                const reaction = collected.first();
+                if( tail && reaction.emoji.name === '📀'){
+                    sentmsg.channel.send("it's tail (📀) ! Congratulations <@"+ msg.author.id +">, you won  !");
+                } else if (tail && reaction.emoji.name === '💿') {
+                    sentmsg.channel.send("it's tail (📀) but you chose head (💿) ! You lost <@"+ msg.author.id +"> !");
+                } else if (head && reaction.emoji.name === '📀') {
+                    sentmsg.channel.send("it's head (💿) but you chose tail (📀)! You lost <@"+ msg.author.id +"> !");
+                } else if (head && reaction.emoji.name === '💿') {
+                    sentmsg.channel.send("it's head (💿) ! Congragulations <@"+ msg.author.id +">, you won !");
+                }
+
+            })
+            .catch(collected => {
+                msg.channel.send("<@"+ msg.author.id +">, you took too much time to respond or reacted with something else");
+            });
+        }).catch(collected => {
+            msg.channel.send('an error occured please try later :D ');
+        });
+        
+    
+    }
+
+    if(cmd == "pf" || cmd == "pileouface"){
+        msg.reply("je vais lancer une pièce. Quelle face choisissez vous ? \n 💿 = Face \n 📀 = Pile ").then(sentmsg => {
+            sentmsg.react('💿'); 
+            sentmsg.react('📀');
+
+            const filter = (reaction, user) => {
+                return ['💿','📀'].includes(reaction.emoji.name) && user.id === msg.author.id;
+            };
+
+            sentmsg.awaitReactions(filter,{max:1, time: waitingTime,error: ['time']})
+            .then(collected => {
+                var array = [] ;
+                for(var i = 0; i<1000; i++){
+                    array.push(Math.random());
+                }
+
+                var rdval = array[Math.floor(Math.random()* Math.floor(999))];
+                var tail = rdval < 0.5;
+                var head = rdval > 0.5;
+
+                if(tail){
+                    sentmsg.channel.send("c'est tombé sur pile (📀)");
+                } else {
+                    sentmsg.channel.send("c'est tombé sur face (💿)");
+                }
+
+                const reaction = collected.first();
+                if( tail && reaction.emoji.name === '📀'){
+                    sentmsg.channel.send("c'est pile (📀) ! Bravo <@"+ msg.author.id +">, vous avez gagné !");
+                } else if (tail && reaction.emoji.name === '💿') {
+                    sentmsg.channel.send("c'est pile (📀) dommage <@"+ msg.author.id +">, vous avez choisi face (💿) ! Perdu !");
+                } else if (head && reaction.emoji.name === '📀') {
+                    sentmsg.channel.send("c'est face (💿) mais vous avez choisi pile (📀)! Dommage <@"+ msg.author.id +">, c'est perdu !");
+                } else if (head && reaction.emoji.name === '💿') {
+                    sentmsg.channel.send("c'est face (💿) ! Bravo <@"+ msg.author.id +">, vous avez gagné !");
+                }
+
+            })
+            .catch(collected => {
+                msg.channel.send("<@"+ msg.author.id +">, vous avez pris trop de temps ou vous avez réagi avec quelque chose d'innatendu");
+            });
+        }).catch(collected => {
+            msg.channel.send('il y a eu une erreur ré-essayez plus tard :D ');
+        });
+        
+    
+    }
+
+
+});
+
+SideBot.login(Config.Token);
+
+
